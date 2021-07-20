3c12905ba104d1f7869155e387e638c601e8c523
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-07-20 20:06:45 +0500

    lottery contract implemented with react js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,81 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {
+    getManager();
+    getPlayers();
+    getBalance();
+  }, []);
+
+  const getPlayers = async () => {
+    const players = await lottery.methods.getPlayers().call();
+    setPlayers(players);
+  };
+
+  const getBalance = async () => {
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    setBalance(balance);
+  };
+
+  const getManager = async () => {
+    const manager = await lottery.methods.manager().call();
+    setManager(manager);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage("Waiting on transaction success...");
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, "ether"),
+    });
+    setMessage("you have been entered!!");
+  };
+
+  const pickWinner = async () => {
+    const accounts = await web3.eth.getAccounts();
+    setMessage("Waiting on transaction success...");
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+    setMessage("Winner has been picked!!");
+  };
+
+  const [manager, setManager] = useState("");
+  const [balance, setBalance] = useState("");
+  const [players, setPlayers] = useState("");
+  const [value, setvalue] = useState("");
+  const [message, setMessage] = useState("");
+  return (
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This Contract is managed by {manager}</p>
+      <p>
+        There are currently {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")}
+      </p>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value={value}
+            onChange={(event) => setvalue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+      <hr />
+      <h4>ready to pick a winner?</h4>
+      <button onClick={pickWinner}>Pick a Winner</button>
+      <hr />
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
 export default App;
